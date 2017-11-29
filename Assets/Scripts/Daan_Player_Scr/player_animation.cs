fef086c6b0f09cc26fee2d16dfb5481c62be8f4b
Daan Ruting <daan.j.ruting@gmail.com>
2017-11-29 13:19:59 +0100

    Started making player scripts and scripting those

---
+++ Assets/Scripts/Daan_Player_Scr/player_animation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_animation : MonoBehaviour
+{
+    private float _animationTime;
+
+	void Start() {
+		
+	}
+	
+	void Update() {
+		
+	}
+    public void PlayAnimation()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Daan_Player_Scr/player_mesh.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_mesh : MonoBehaviour
+{
+    public MeshCollider cl;
+    public GameObject playerObject;
+    private player _player; 
+
+	void Start()
+    {
+        _player = playerObject.GetComponent<player>();
+	}
+}
