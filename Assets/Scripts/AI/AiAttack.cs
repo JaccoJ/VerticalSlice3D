a4c4fe9e6ac1b52140dff86faea31802a34ffb12
JaccoJongenotter <22053@ma-web.nl>
2017-11-30 10:51:50 +0100

    Scripts en Scene

---
+++ Assets/Scripts/AI/AiAttack.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AiAttack : MonoBehaviour {
+
+    public GameObject kogelSpawn;
+    public GameObject kogelPrefab;
+    //public GameObject Player;
+    // Use this for initialization
+    [SerializeField]
+    private float kogelspeed;
+    public Transform player2;
+
+
+   
+
+    public void Shoot()
+    {
+        
+        GameObject kogel;
+        kogel = Instantiate(kogelPrefab, kogelSpawn.transform.position, kogelSpawn.transform.rotation) as GameObject;
+
+        Rigidbody kogelRigidbody;
+        kogelRigidbody = kogel.GetComponent<Rigidbody>();
+
+        kogelRigidbody.AddForce((player2.position - transform.position) * kogelspeed );
+
+        Destroy(kogel, 0.50f);
+    }
+
+}
---
+++ Assets/Scripts/AI/AiRecoil.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AiRecoil : MonoBehaviour {
+
+    private Rigidbody rb;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+
+
+        if (col.gameObject.tag == "Player")
+        {
+            print("hit");
+            rb.AddForce(new Vector3(-6,0,0) );
+        }
+    }
+}
