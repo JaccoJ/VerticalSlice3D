1d7371094d67e2188ccd5c8b6362cc376de0b4af
RemcoRemerij <r.remery@hotmail.com>
2017-12-02 22:51:25 +0100

    Revert "Revert "Merge branch 'master' of https://github.com/JaccoJ/VerticalSlice3D""
    This reverts commit 54cd7d459ec46dd8e8ccd172f4aa3e3e77b1f62d.

---
+++ Assets/Scripts/TestScripts/TestMoveScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMoveScript : MonoBehaviour
+{
+    private InputManager inputManager;
+    [SerializeField]
+    private float movementSpeed = 5;
+
+    [SerializeField]
+    private Transform _testPlayer;
+
+
+    void Start()
+    {
+       
+        if (!(inputManager = this.GetComponent<InputManager>()))
+        {
+            inputManager = this.gameObject.AddComponent<InputManager>();
+        }
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            this.transform.Rotate(0, 1, 0);
+        }
+        Vector3 movement = new Vector3();
+        if (inputManager.Up())
+        {
+            movement += this.transform.forward;
+        }
+        if (inputManager.Down())
+        {
+            movement -= this.transform.forward;
+        }
+        if (inputManager.Right())
+        {
+            movement += this.transform.right;
+        }
+        if (inputManager.Left())
+        {
+            movement -= this.transform.right;
+        }
+        movement.Normalize();
+        this.transform.position += (movement * Time.deltaTime * movementSpeed);
+    }
+
+    
+}
