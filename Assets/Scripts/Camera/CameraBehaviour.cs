8301335dc6822a21784f5faba31e1bcbd8d63fb5
JeffrinDumas <jeffrin.e.d@live.nl>
2017-12-06 13:54:17 +0100

    starting on camera. angle calculation finished

---
+++ Assets/Scripts/Camera/CameraBehaviour.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _target;
+    [SerializeField]
+    private GameObject _player;
+
+    private Transform _cameraTransform;
+
+    private bool _LookingAt;
+
+    private float _distancePlayerCamera;
+    
+   
+
+    private Vector3 _crosspointLookAt;
+
+
+	void Start ()
+    {
+       
+        _cameraTransform = Camera.main.transform;
+        _LookingAt = false;
+        
+        
+	}
+	
+	
+	void Update ()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            _LookingAt = !_LookingAt;
+        }
+
+        if (_LookingAt == true)
+        {
+
+            _cameraTransform.LookAt(_target.transform);
+            Vector3 CameraForward = Camera.main.transform.forward;
+            Vector3 _distancePlayerEnemy = _target.transform.position - _player.transform.position;
+            float angle = Vector3.Angle(CameraForward, _distancePlayerEnemy);
+        }
+        else
+        {
+            _cameraTransform.LookAt(_player.transform);
+        }
+
+       
+    }
+}
